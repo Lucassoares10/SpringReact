b67672b120e013a057ff09993d0f19d1654c18c2
Lucas Da Silva Soares (Basis) <lucassoares@mec.gov.br>
2021-09-09 20:11:56 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DataTable from 'compents/DataTable';
+import Foorter from 'compents/Footer';
+import Navbar from 'compents/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1 className="container">Pagina de vendas</h1>
+      <DataTable  />
+      </div>
+      <Foorter  />
+    </>
   );
 }
 
---
+++ frontend/src/compents/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Foorter() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por Lucas da Silva Soares<a href="https://github.com/Lucassoares10" target="_blank" rel="noreferrer">Lucas Soares</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Foorter;
