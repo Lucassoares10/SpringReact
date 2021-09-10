cbe873e83201a2e7e00f3080ad73b539a0d959ec
Lucas Da Silva Soares (Basis) <lucassoares@mec.gov.br>
2021-09-09 21:30:45 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'compents/BarChart';
 import DataTable from 'compents/DataTable';
+import DonutChart from 'compents/DonutChart';
 import Foorter from 'compents/Footer';
 import Navbar from 'compents/Navbar';
 
@@ -6,11 +8,33 @@ function App() {
   return (
     <>
       <Navbar />
-      <div>
-        <h1 className="container">Pagina de vendas</h1>
-      <DataTable  />
+      <div className="container">
+
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row">
+
+          <div className="row px-3">
+            <div className="col-sm-6">
+                <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+              <BarChart />
+                </div>
+                <div className="col-sm-6">
+                <h5>Todas Vendas</h5>
+              <DonutChart />
+                </div>
+          </div>
+
+        </div>
+        
+        <div className="py-3">
+         <h2 className="text-primary">
+          Todas as vendas
+         </h2>
+        </div>
+
+        <DataTable />
       </div>
-      <Foorter  />
+      <Foorter />
     </>
   );
 }
